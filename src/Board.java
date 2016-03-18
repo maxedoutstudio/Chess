3631112c87f9cf93a8822fbaf84ca5e4610eba5b
maxedoutstudio <max@maxedoutstudio.com>
2016-03-18 18:05:50 -0400

    Completed saving and loading from file.

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -29,13 +32,13 @@ public class Board {
 	
 	//Saving and loading
 	
-	public boolean saveFile(String fileName){
+	public void saveFile(String fileName) throws IOException,FileNotFoundException{
 		
 		File file = new File(fileName);
 		
 		try{
 		
-			FileWriter writer = new FileWriter(fileName);
+			FileWriter writer = new FileWriter(file);
 			
 			
 			for(int i = 0;i<board.length;i++){
@@ -56,9 +59,53 @@ public class Board {
 			}
 			
 			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-			
+			e.printStackTrace();
 		}
+		
+
+	}
+	
+	public void loadFile(String fileName) throws IOException,FileNotFoundException{
+		
+		File file = new File(fileName);
+		
+		Piece[][] newPieces = new Piece[BOARD_SIZE][BOARD_SIZE];
+		
+		int rowCounter = 0;
+		
+		try{
+		
+			String line;
+			
+			FileReader reader = new FileReader(fileName);
+			
+			BufferedReader br = new BufferedReader(reader);
+			
+			while ((line = br.readLine()) != null) {
+				
+				String[] row = line.split(",");
+				
+				for(int i = 0;i<row.length;i++){
+					String[] parts = row[i].split(" ");
+					newPieces[rowCounter][i] = new Piece(parts[0],parts[1]);
+				}
+				
+				rowCounter++;
+			}
+		
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+			
 	}
 	
 
